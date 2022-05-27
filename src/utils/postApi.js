43cfa6a329e1d6e728c2910d56269ffcac81e6ab
shaybay <pacificconnect310@gmail.com>
2022-05-27 07:02:00 -0700

    trying one last time before i just go diggin a dying hole and die

---
+++ src/utils/postApi.js
@@ -0,0 +1,43 @@
+import tokenService from './tokenService';
+
+const BASE_URL = '/api/posts';
+
+export function create(post) {
+    return fetch(BASE_URL, {
+      method: 'POST',
+      body: post,
+      headers: {
+        'Authorization': 'Bearer ' + tokenService.getToken(),
+      },
+    
+    }).then(res => {
+      if(res.ok) return res.json();
+      throw new Error('CHECK THE SERVER TERMINAL')
+    })
+  }
+
+  export function remove(postId) {
+    console.log('remove', postId);
+    return fetch(`${BASE_URL}/${postId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + tokenService.getToken(),
+      },
+    
+    }).then(res => {
+      if(res.ok) return res.json();
+      throw new Error('CHECK THE SERVER TERMINAL')
+    })
+  }
+
+  export function getAll() {
+    return fetch(BASE_URL, {
+      headers: {
+        'Authorization': 'Bearer ' + tokenService.getToken()
+      }
+    })
+    .then(res => {
+      if(res.ok) return res.json();
+      throw new Error('Bad Credentials! CHECK THE SERVER TERMINAL!')
+    })
+  }
